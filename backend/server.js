056a634a77984062e5a04fc6cdcff71fdc432fcf
faheemkhan <faheemkhan.pg@gmail.com>
2024-08-07 21:13:11 +0500

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
+import path from 'path'
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from './routes/message.routes.js';
@@ -13,9 +14,12 @@ import { app, server } from './socket/socket.js';
 const PORT = process.env.PORT || 5000;
 config()
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
+
 
 // app.get('/',(req,res)=>{
 //     // root route http://localhost:5000
@@ -26,6 +30,11 @@ app.use('/api/auth',authRoutes)
 app.use("/api/message",messageRoutes)
 app.use("/api/users",userRouter)
 
+app.use(express.static(path.join(__dirname,'/chat-app/dist')))
+
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname,"frontend", 'dist',"index.html"))
+})
 
 server.listen(PORT,() =>{
     connectToMongoDb();
